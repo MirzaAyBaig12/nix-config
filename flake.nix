e727067ee9ea8a79fbedfe25bfe31b479e469df2
MirzaAyBaig12 <mirzaaybaig12@gmail.com>
2026-08-03 23:38:42 -0700

    Initial Axiom system profile setup

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "My NixOS Configuration Flake";
+
+  inputs = {
+    # 1. Core NixOS package repository
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    
+    # 2. Third-party Snap support for NixOS
+    nix-snapd.url = "github:nix-community/nix-snapd";
+    nix-snapd.inputs.nixpkgs.follows = "nixpkgs";
+    
+    #nix software center
+    nix-software-center.url = "github:snowfallorg/nix-software-center";
+    nix-software-center.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, nix-snapd, ... }@inputs: {
+    nixosConfigurations = {
+      # ⚠️ Replace "nixos" with your actual system hostname if it's different!
+      Axiom = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          # Links your local configuration.nix file
+          ./configuration.nix 
+
+          # Pulls in the Snap module from the nix-snapd repository
+          nix-snapd.nixosModules.default 
+          
+          # Enables the snap service inline
+          {
+            services.snap.enable = true;
+          }
+        ];
+      };
+    };
+  };
+}
+
