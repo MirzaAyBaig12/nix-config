c9365f24c67b174f533e11b09d9eb974577753d6
Комиссар Блятников <MirzaAyBaig12@gmail.com>
2026-08-05 02:58:05 -0700

    update 2026-08-05_02:58

--- configuration.nix
+++ configuration.nix
@@ -1,4 +1,4 @@
-# Edit this configuration file to define what should be installed on
+    # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
@@ -143,7 +143,6 @@
     description = "Ayaan Mirza";
     extraGroups = [ "networkmanager" "wheel" ];
     shell = pkgs.zsh;
-    ];
   };
 
   # Install librewolf (via the firefox module, package overridden)
