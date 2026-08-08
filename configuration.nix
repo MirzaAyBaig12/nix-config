ff4fb819aeacf234e4339eea3e7c7b295db585bd
Комиссар Блятников <MirzaAyBaig12@gmail.com>
2026-08-08 14:39:10 -0700

    update 2026-08-08_14:39

--- configuration.nix
+++ configuration.nix
@@ -401,12 +401,12 @@
 
   }
   
-  programs.steam = {
+programs.steam = {
     enable = true; # Master switch, already covered in installation
     remotePlay.openFirewall = true;  # Open ports in the firewall for Steam Remote Playmodern
     dedicatedServer.openFirewall = true; # Open ports for Source Dedicated Server hosting
     # Other general flags if available can be set here.
-  };
+};
 
   programs = {
      zsh = {
