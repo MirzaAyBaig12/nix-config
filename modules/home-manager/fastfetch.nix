5c4d573c45334e5434c7e5073f1db07725807f75
Комиссар Блятников <MirzaAyBaig12@gmail.com>
2026-08-10 16:17:31 -0700

    Use proper path for config files

--- modules/home-manager/fastfetch.nix
+++ modules/home-manager/fastfetch.nix
@@ -2,6 +2,6 @@
 {
   home.packages = [ pkgs.fastfetch ];
 
-  xdg.configFile."fastfetch/config.jsonc".source = ~/nix-config/.config/fastfetch/config.jsonc;
-  xdg.configFile."fastfetch/compact-config.jsonc".source = ~/nix-config/.config/fastfetch/compact-config.jsonc;
+  xdg.configFile."fastfetch/config.jsonc".source = ../../.config/fastfetch/config.jsonc;
+  xdg.configFile."fastfetch/compact-config.jsonc".source = ../../.config/fastfetch/compact-config.jsonc;
 }
