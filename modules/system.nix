0178d89351b4148908fa5e46c7c018923fcf19d6
Комиссар Блятников <MirzaAyBaig12@gmail.com>
2026-08-11 04:48:37 -0700

    Enable GRUB

--- modules/system.nix
+++ modules/system.nix
@@ -14,9 +14,9 @@
   };
 
   # Bootloader setup (systemd-boot + rEFInd + Secure Boot signing)
-  boot.loader.grub.enable = false;
+  boot.loader.grub.enable = true;
   boot.loader.systemd-boot = {
-    enable = true; 
+    enable = false; 
     configurationLimit = 5;
     extraInstallCommands = ''
       echo "auto-entries no" >> /boot/loader/loader.conf
