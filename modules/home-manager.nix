eb5fe88773b2d67fc0ed8363d77e3b5f1a69f474
Комиссар Блятников <MirzaAyBaig12@gmail.com>
2026-08-11 00:33:57 -0700

    Create programs module for home-manager

--- modules/home-manager.nix
+++ modules/home-manager.nix
@@ -8,6 +8,7 @@
     users.ayaan_mirza = {
       imports = [
         #imports from home-manager modules
+        ./home-manager/programs.nix
         ./home-manager/fastfetch.nix
         ./home-manager/zsh.nix
       ];
