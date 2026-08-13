b4e59b9d84bad5bfa2901cd392736b05d6823670
Комиссар Блятников <mirzaaybaig12@protonmail.com>
2026-08-13 14:13:27 -0700

    Fix Error

--- modules/programs.nix
+++ modules/programs.nix
@@ -110,6 +110,6 @@
     vscode #Visual Studio Code
     zed-editor #Zed Editor
     distrobox #Distrobox for running containers
-    ksshaskpass #KDE SSH Password Prompt
+    kdePackages.ksshaskpass #KDE SSH Password Prompt
   ];
 }
