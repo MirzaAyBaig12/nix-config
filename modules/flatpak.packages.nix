0af3d5754f28c37aaedbcd637aac3ef31ae94e71
Комиссар Блятников <Mirzaaybaig12@protonmail.com>
2026-08-14 15:42:02 -0700

    Add net.cozic.joplin_desktop

--- modules/flatpak.packages.nix
+++ modules/flatpak.packages.nix
@@ -29,6 +29,7 @@
     "io.missioncenter.MissionCenter"
     "io.podman_desktop.PodmanDesktop"
     "it.mijorus.gearlever"
+    "net.cozic.joplin_desktop"
     "org.bleachbit.BleachBit"
     "org.desktop_plus.desktop-plus"
     "org.equicord.equibop"
