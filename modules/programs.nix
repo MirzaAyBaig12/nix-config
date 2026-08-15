e245c2e3e68d0e2ed90023844081464d27124488
Комиссар Блятников <Mirzaaybaig12@protonmail.com>
2026-08-14 21:38:04 -0700

    Change NH Preferences

--- modules/programs.nix
+++ modules/programs.nix
@@ -10,7 +10,7 @@
   programs.nh = {
     enable = true;
     clean.enable = true;
-    clean.extraArgs = "--keep-since 4d --keep 3";
+    clean.extraArgs = "--keep 3";
     flake = "/home/ayaan_mirza/nix-config"; # sets NH_OS_FLAKE variable for you
   };
 
