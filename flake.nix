376c8b1acdb2bb30013a33ccb5ec776dea435189
Комиссар Блятников <Mirzaaybaig12@protonmail.com>
2026-08-18 13:06:03 -0700

    Add Ventoy to Permitted Insecure Packages

--- flake.nix
+++ flake.nix
@@ -4,17 +4,18 @@
   nixConfig = {
       extra-substituters = [ "https://look.cachix.org" ];
       extra-trusted-public-keys = [ "look.cachix.org-1:8elPCeSVBzlDZXqIRKBK9GyLIK/Hoe1xiWZF0ir7uX4=" ];
+      extra-deprecated-features = [ "or-as-identifier" ];
     };
 
   inputs = {
 
     # 1. Core NixOS package repository
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    
+
     # 2. Third-party Snap support for NixOS
     nix-snapd.url = "github:nix-community/nix-snapd";
     nix-snapd.inputs.nixpkgs.follows = "nixpkgs";
-    
+
     # 3. Nix Software Center
     nix-software-center.url = "github:xinux-org/software-center";
 
@@ -37,7 +38,7 @@
     # 8. WinPodX
     winpodx.url = "github:kernalix7/winpodx";
     winpodx.inputs.nixpkgs.follows = "nixpkgs";
-    
+
     # 9. NixOS Conf Editor
     nixos-conf-editor.url = "github:snowfallorg/nixos-conf-editor";
 
@@ -50,7 +51,7 @@
     # 12. Look Launcher (Cachix)
     look.url = "github:kunkka19xx/look?dir=apps/linows";
 
-    # 13. Home-Manager 
+    # 13. Home-Manager
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -81,18 +82,18 @@
     nixosConfigurations = {
       Axiom = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        
+
         specialArgs = { inherit inputs; };
-        
+
         modules = [
           # Links your local configuration.nix file
-          ./configuration.nix 
+          ./configuration.nix
 
           # Pulls in the Snap module from the nix-snapd repository
-          nix-snapd.nixosModules.default 
+          nix-snapd.nixosModules.default
 
           # Codex Desktop (ChatGPT Desktop) NixOS module
-          codex-desktop-linux.nixosModules.default 
+          codex-desktop-linux.nixosModules.default
 
           # Declarative Flatpak module
           nix-flatpak.nixosModules.nix-flatpak
@@ -117,6 +118,13 @@
           # Tags each generation with the git revision it was built from
           {
             system.configurationRevision = self.rev or self.dirtyRev or "dirty";
+          }
+
+          # Explicitly permits the insecure ventoy-full package
+          {
+            nixpkgs.config.permittedInsecurePackages = [
+              "ventoy-qt-1.1.17"
+            ];
           }
         ];
       };
