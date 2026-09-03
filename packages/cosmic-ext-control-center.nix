0b03ad610e26156f96a3fc419c1268938acbf604
Комиссар Блятников <mirzaaybaig12@protonmail.com>
2026-09-03 03:18:21 -0700

    Update hash

--- packages/cosmic-ext-control-center.nix
+++ packages/cosmic-ext-control-center.nix
@@ -40,7 +40,7 @@ rustPlatform.buildRustPackage rec {
     # Placeholder — `nixos-rebuild` will fail on the first build with the
     # real hash ("got: sha256-...") once you save this file. Paste it here
     # and rebuild again. Do this again any time you bump `rev`.
-    hash = "sha256-Wc1av079Y4ZNcGp6uf7KAGPeBd/joDeS0ppwe+zR3bo=";
+    hash = "sha256-mwimFnEv6QHl7Ury+7fv1EMrWejjEewXTOlFauO4RN0=";
   };
 
   cargoLock = {
