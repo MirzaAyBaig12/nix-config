5e90a7894e6c28f2289a3a4b08d769e2a014e62e
Комиссар Блятников <mirzaaybaig12@protonmail.com>
2026-09-06 04:32:56 -0700

    Update Flatpak packages sync

--- modules/flatpak.packages.nix
+++ modules/flatpak.packages.nix
@@ -1,10 +1,12 @@
 {
   flathub = [
+    "app.devsuite.Ptyxis"
     "com.freerdp.FreeRDP"
     "com.github.IsmaelMartinez.teams_for_linux"
     "com.github.robertsanseries.ciano"
     "com.github.tchx84.Flatseal"
     "com.github.unrud.VideoDownloader"
+    "com.sidevesh.Luminance"
     "com.spotify.Client"
     "com.vysp3r.ProtonPlus"
     "dev.edfloreshz.CosmicTweaks"
@@ -16,6 +18,7 @@
     "io.github.kolunmi.Bazaar"
     "io.github.nacho.mecalin"
     "io.github.shiftey.Desktop"
+    "io.github.shonubot.Spruce"
     "io.github.streetpea.Chiaki4deck"
     "io.github.ungoogled_software.ungoogled_chromium"
     "io.gitlab.adhami3310.Impression"
@@ -30,9 +33,13 @@
     "org.kde.isoimagewriter"
     "org.pvermeer.WebAppHub"
     "uk.co.cappsy.Tesseract"
+    "xyz.riothedev.emojify"
   ];
 
   cosmic = [
+    { appId = "com.championpeak87.cosmic-ext-classic-menu"; origin = "cosmic"; }
+    { appId = "com.github.MiracleForge.cosmic-ext-marketwatch"; origin = "cosmic"; }
+    { appId = "com.github.bgub.CosmicExtAppletPomodoro"; origin = "cosmic"; }
     { appId = "com.github.bgub.CosmicExtAppletVigil"; origin = "cosmic"; }
     { appId = "com.vintagetechie.CosmicExtAppletTempest"; origin = "cosmic"; }
     { appId = "dev.cappsy.CosmicExtAppletLogoMenu"; origin = "cosmic"; }
