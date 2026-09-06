daaab7985c6f78908852d08ea3f59fb0dcbd250c
Комиссар Блятников <mirzaaybaig12@protonmail.com>
2026-09-06 02:04:22 -0700

    Install Flameshot

--- modules/home-manager/programs.hm.nix
+++ modules/home-manager/programs.hm.nix
@@ -7,4 +7,19 @@
     nixd
     nixfmt
   ];
+
+  services.flameshot = {
+    # Also installs/enables flameshot
+    enable = true;
+    settings = {
+      General = {
+        useGrimAdapter = true;
+        # Stops warnings for using Grim
+        disabledGrimWarning = true;
+    };
+  };
+};
+
+  # DankSearch — file search plugin powering DMS's launcher results
+  programs.dsearch.enable = true;
 }
