e251ac227700de428b81bbfdc00d775490005925
Комиссар Блятников <mirzaaybaig12@protonmail.com>
2026-09-12 21:31:02 -0700

    Add org.onlyoffice.desktopeditors

--- modules/flatpak.packages.nix
+++ modules/flatpak.packages.nix
@@ -28,6 +28,7 @@
     "org.gnome.Boxes"
     "org.gnome.Chess"
     "org.kde.isoimagewriter"
+    "org.onlyoffice.desktopeditors"
     "org.pvermeer.WebAppHub"
     "page.codeberg.grinka.Transition"
     "uk.co.cappsy.Tesseract"
