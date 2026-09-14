165e14f6fcdeafaac6c30951b38984a03138c6a5
Комиссар Блятников <mirzaaybaig12@protonmail.com>
2026-09-14 00:35:30 -0700

    Add org.mozilla.thunderbird_esr

--- modules/flatpak.packages.nix
+++ modules/flatpak.packages.nix
@@ -27,6 +27,7 @@
     "org.gnome.Loupe"
     "org.gnome.Maps"
     "org.kde.isoimagewriter"
+    "org.mozilla.thunderbird_esr"
     "org.pvermeer.WebAppHub"
     "page.codeberg.grinka.Transition"
     "uk.co.cappsy.Tesseract"
