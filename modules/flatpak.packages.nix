b38e90925c571fe6bb5a309709876b70787ce551
Комиссар Блятников <mirzaaybaig12@protonmail.com>
2026-09-14 01:02:45 -0700

    Add net.codelogistics.webapps

--- modules/flatpak.packages.nix
+++ modules/flatpak.packages.nix
@@ -23,6 +23,7 @@
     "io.gitlab.adhami3310.Impression"
     "io.missioncenter.MissionCenter"
     "io.podman_desktop.PodmanDesktop"
+    "net.codelogistics.webapps"
     "org.bleachbit.BleachBit"
     "org.equicord.equibop"
     "org.gnome.Chess"
